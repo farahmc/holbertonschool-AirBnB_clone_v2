ab58634dad1b7d7b07b14f466791c6b4fd5c93df
farahmc <86407866+farahmc@users.noreply.github.com>
2022-08-10 15:45:29 +1000

    Update test_base_model.py

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -107,7 +107,3 @@ class test_fileStorage(unittest.TestCase):
         from models.engine.file_storage import FileStorage
         print(type(storage))
         self.assertEqual(type(storage), FileStorage)
-
-
-if __name__ == '__main__':
-    unittest.main()
