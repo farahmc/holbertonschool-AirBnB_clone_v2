f5635f7155639aa585a4a2779c9b7e973771c20f
Farah <farah.mccurdy@gmail.com>
2022-08-25 03:33:20 -0700

    added upload, uncompress and delete archive

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,13 +6,25 @@ from fabric.api import *
 env.hosts = ['3.92.23.198', '54.174.101.66']
 env.user = 'ubuntu'
 
+
 def do_deploy(archive_path):
     """ Distribute an archive to your web servers"""
     if os.path.exists(archive_path):
         return False
     try:
-        """ upload archive """
+        """ upload archive & uncompress"""
         put(archive_path, "/tmp/")
+        archive_path_split = archive_path.split("/")[1]
+        filename = archive_path_split.split(".")[0]
+        run("mkdir -p /data/web_static/releases/{}".format(filename))
+        run("tar -xf /tmp/{} -C /data/web_static/releases/{}".
+            format(archive_path_split, filename)
+
+        """ delete archive """
+        run("rm /tmp/{}".format(archive_path_split))
+
+        """ delete symlink """
+        run("rm -rf /data/web_static/current")
 
     except:
         return False
