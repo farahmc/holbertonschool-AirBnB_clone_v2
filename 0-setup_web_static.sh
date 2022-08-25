dfc79ec41b2f05cdc0b29c43655898cdb0b86aaa
Farah <farah.mccurdy@gmail.com>
2022-08-24 21:42:03 -0700

    change: move change permissions to further down

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -13,11 +13,11 @@ mkdir -p "/data/web_static/shared/"
 # create symlink (delete if exists first)
 ln -s -f -n /data/web_static/releases/test/ /data/web_static/current
 
-# set permissions
-chown -R ubuntu:ubuntu /data/
-
 # create a & populate dummy index
 printf "Success!" > /data/web_static/releases/test/index.html
+
+# set permissions
+chown -R ubuntu:ubuntu /data/
 
 # update nginx config
 sed -i '/add_header X-Served-By/a location /hbnb_static { \n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
