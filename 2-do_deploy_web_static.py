dafe75a4c8258d9019b9707926b5c52e07e1cd11
Farah <farah.mccurdy@gmail.com>
2022-08-25 04:16:27 -0700

    change: remove first / on paths, slice last for filename

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -12,22 +12,22 @@ def do_deploy(archive_path):
         return False
     try:
         """ upload archive """
-        put(archive_path, "/tmp/")
+        put(archive_path, "tmp")
 
         """ create directory and uncompress """
-        archive_path_split = archive_path.split("/")[1]
+        archive_path_split = archive_path.split("/")[-1]
         filename = archive_path_split.split(".")[0]
-        directory = "/data/web_static/releases/" + filename
+        directory = "data/web_static/releases/" + filename
         run("mkdir -p {}".format(directory))
-        run("tar -xzf /tmp/{} -C {}".
+        run("tar -xzf tmp/{} -C {}".
             format(archive_path_split, directory))
 
         """ delete archive """
-        run("rm /tmp/{}".format(archive_path_split))
+        run("rm tmp/{}".format(archive_path_split))
 
         """ delete symlink & create new symlink"""
-        run("rm -rf /data/web_static/current")
-        run("ln -s {} /data/web_static/current".
+        run("rm -rf data/web_static/current")
+        run("ln -s {} data/web_static/current".
             format(directory))
 
     except:
