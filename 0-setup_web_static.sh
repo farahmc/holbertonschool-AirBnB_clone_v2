5c7f5b194008aa387c24d1f45aab799747a6f31e
Farah <farah.mccurdy@gmail.com>
2022-08-24 21:22:42 -0700

    change: sed find where to append

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -12,18 +12,12 @@ mkdir -p "/data/web_static/shared/"
 chown -R ubuntu:ubuntu /data/
 
 # create symlink (delete if exists first)
-ln -s -f /data/web_static/releases/test/ /data/web_static/current
+ln -s -f -n /data/web_static/releases/test/ /data/web_static/current
 
 # create a & populate dummy index
-printf "<html>
-		<head>
-		</head>
-		<body>
-			Hello World
-		</body>
-	</html>" > /data/web_static/releases/test/index.html
+printf "Success!" > /data/web_static/releases/test/index.html
 
 # update nginx config
-sed -i '/server_name _;/a location /hbnb_static {alias /data/web_static/current/};' /etc/nginx/sites-available/default
+sed -i '/add_header X-Served-By/a \nlocation /hbnb_static { \n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
 
 service nginx restart
