48cb20592a32924c1ca682b0f8f9833447d8a947
Farah <farah.mccurdy@gmail.com>
2022-08-25 17:08:00 -0700

    change: early return typo

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -8,7 +8,7 @@ env.hosts = ['3.92.23.198', '54.174.101.66']
 
 def do_deploy(archive_path):
     """ Distribute an archive to your web servers"""
-    if os.path.exists(archive_path):
+    if not os.path.exists(archive_path):
         return False
     try:
         """ upload archive """
@@ -27,8 +27,7 @@ def do_deploy(archive_path):
 
         """ delete symlink & create new symlink"""
         run("rm -rf /data/web_static/current")
-        run("ln -s {} /data/web_static/current".
-            format(directory))
+        run("ln -s {} /data/web_static/current".format(directory))
 
     except:
         return False
